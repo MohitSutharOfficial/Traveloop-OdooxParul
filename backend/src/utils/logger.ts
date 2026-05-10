9b304a343b70975cac965a54fcbb76a3a995e258
Mohit Suthar <kingmohitsuthar@gmail.com>
2026-05-10 16:35:00 +0530

    fixing th cors

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -20,14 +20,19 @@ const format = winston.format.combine(
   )
 );
 
-const transports = [
+const transports: winston.transport[] = [
   new winston.transports.Console(),
-  new winston.transports.File({
-    filename: 'logs/error.log',
-    level: 'error',
-  }),
-  new winston.transports.File({ filename: 'logs/all.log' }),
 ];
+
+if (!process.env.VERCEL) {
+  transports.push(
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'error',
+    }),
+    new winston.transports.File({ filename: 'logs/all.log' })
+  );
+}
 
 export const logger = winston.createLogger({
   level: logLevel,
